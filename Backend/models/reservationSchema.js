51e9617be3e4ebef3af09cae3906ce3bf81086b7
Harshita Tyagi <imharshita30@gmail.com>
2026-02-08 12:39:18 +0530

    Restaurant Reservation Project

---
+++ Backend/models/reservationSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const reservationSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First name must be of at least 3 Characters."],
+    maxLength: [30, "First name cannot exceed 30 Characters."],
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last name must be of at least 3 Characters."],
+    maxLength: [30, "Last name cannot exceed 30 Characters."],
+  },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Provide a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    // FIXED: length 10 kar di hai
+    minLength: [10, "Phone number must contain 10 Digits."],
+    maxLength: [10, "Phone number must contain 10 Digits."],
+  },
+});
+
+export const Reservation = mongoose.model("Reservation", reservationSchema);
